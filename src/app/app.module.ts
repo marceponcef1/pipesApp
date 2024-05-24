e3bcd0d575332d7528a4bfa0949770ee166236cb
Marcelino Ponce Fajardo <mponce@iodigital.es>
2024-05-24 11:25:05 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,21 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+
+// Configuración del 'locale' de la app
+import localeEsHn from '@angular/common/locales/es-HN';
+import localeFrCa from '@angular/common/locales/fr-CA';
+import { registerLocaleData } from '@angular/common';
+
+
+registerLocaleData(localeEsHn);
+registerLocaleData(localeFrCa);
+
 
 @NgModule({
   declarations: [
@@ -10,9 +23,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-HN'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
